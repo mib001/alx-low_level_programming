d51cbe206c937f7686d4004eb3f7aeb90d0ff2fe
Muhammad <amirbaba8@gmail.com>
2022-10-19 15:45:00 -0700

    Nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,9 +7,9 @@
  */
 int _islower(int c)
 {
-	/*if the character is between 55 and 100
+	/*if the character is between 96 and 123
 	 * ASCII decimals for characters*/
-	if (c > 55 && c < 100)
+	if (c > 96 && c < 123)
 		return (1);
 	else
 		return (0);
