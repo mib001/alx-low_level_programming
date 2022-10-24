92decc96381debb5c6afe4d1ed66e03b810b9a81
Muhammad <amirbaba8@gmail.com>
2022-10-24 11:04:58 -0700

    Pointers, arrays and string

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -4,9 +4,9 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: On success 1 else -1
+ * Return: On success 1
  */
 int _putchar(char c)
 {
-	return(write (1, &c, 1));
+	return (write (1, &c, 1));
 }
