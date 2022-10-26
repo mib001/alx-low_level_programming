6c6346afe9a8ff1bad8fcf318c8401ff67d344e1
Muhammad <amirbaba8@gmail.com>
2022-10-26 11:24:32 -0700

    PAS

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _stcmp - compares two strings
+ * strcmp - compares two strings values
  * @s1: string 1
  * @s2: string 2
  * Return: the difference in s1 and s2
@@ -10,12 +10,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	int i = 0;
+	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] != s1[i])
 		{
-			return (s1[i] - s2[i]);
+			return (s1[i] - s2[i])
 		}
 	i++;
 	}
