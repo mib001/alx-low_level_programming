76e00fbea69eb5c9e828aece7f3ced1128351b92
Muhammad <amirbaba8@gmail.com>
2022-10-31 04:01:33 -0700

    More PAS

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - memory copy src to dest
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes to be copied
+ * Return: string copied
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0, a < n, a++)
+		dest[a] = src[a];
+	return (dest);
+}
