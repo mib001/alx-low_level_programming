1315ace45126b5d7b395c19d6ab44acceba8bb67
Muhammad <amirbaba8@gmail.com>
2022-11-04 13:41:31 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-
 /**
  * main - function to print name
  * @argc: argc parameter
  * @argv: argv parameter
  * Return: 0 for success
  */
-int main (int argc, char *argv[])
+int main (int argc, char **argv[])
 {
 	(void)argc;
 	
