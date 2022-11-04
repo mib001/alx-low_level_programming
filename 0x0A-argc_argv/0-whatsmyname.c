7c3432f61a0017abbfcb6d1cc9e947eb2e475e4f
Muhammad <amirbaba8@gmail.com>
2022-11-04 13:23:47 -0700

    argv and argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - function to print name
+ * @argc: argc parameter
+ * @argv: argv parameter
+ * Return: 0 for success
+ */
+int main (int argc, char *argv[])
+{
+	print("&s\n", *argv);
+	return (0);
+}
