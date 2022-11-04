a6466141b0a1e35699138636d18dc07f95683228
Muhammad <amirbaba8@gmail.com>
2022-11-04 13:58:40 -0700

    argv argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: argv parameter
  * Return: 0 on success
  */
-int main(int argc, char *argv __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
