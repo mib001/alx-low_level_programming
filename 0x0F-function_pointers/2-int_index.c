b0456c86864ed3c8d8c7ff72b28562d3cfa5706e
Muhammad <amirbaba8@gmail.com>
2022-11-16 10:56:55 -0800

    Function ptrs

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int
+ * @array: array to search in
+ * @size: size of array
+ * @cmp: ptr to comparing functions
+ * Return: index of first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+}
