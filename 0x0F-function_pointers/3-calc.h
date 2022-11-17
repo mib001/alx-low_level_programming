6da86cc05fc237401b605d240ce37fb6e8bda7b4
Muhammad <amirbaba8@gmail.com>
2022-11-17 07:58:48 -0800

    Fix

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -17,7 +17,7 @@ typedef struct op
 } op_t;
 
 int op_add(int a, int b);
-int op_sub(int a, imt b);
+int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
